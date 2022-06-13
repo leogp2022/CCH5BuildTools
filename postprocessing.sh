b773bf919ddef05b88447bae7e3735ac0d90d5b3
leo <zihao.liu@dragonplus.com>
2022-06-13 18:25:19 +0800

    add log;

--- postprocessing.sh
+++ postprocessing.sh
@@ -82,6 +82,7 @@ else
     zip -q -r ${dlcZipName} ${dlcName}
     rm -rf ${dlcName}
     mv ${dlcZipName} ${buildPath}/${dlcPath}
+    echo ${remoteUrl}${uploadFileNamePath}/${folder_version}/dlc/${dlcZipName}.zip
     done
 
     echo "DLC End ====================="
